deac158a5e141506d8d180fbf0bcfdf65ad3f92b
Theodore DeRego <tderego94@gmail.com>
2016-01-07 14:53:10 -1000

    re-export ClMatrixMode

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate opencl;
 
 pub use context::Context;
 pub use matrix::Matrix;
-pub use cl_matrix::ClMatrix;
+pub use cl_matrix::{ClMatrix, ClMatrixMode};
 
 pub mod context;
 pub mod cl_matrix;
